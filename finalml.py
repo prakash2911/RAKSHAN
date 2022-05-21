40f70388b1744717f9d9ab0450c66cede2a80185
Aadhith-Ujesh <74974245+Aadhith-Ujesh@users.noreply.github.com>
2022-05-21 11:27:09 +0530

    Update finalml.py

--- finalml.py
+++ finalml.py
@@ -10,9 +10,9 @@ model = load_model('C:/Users/HP/Desktop/CNN_Cat_Dog_Model.h5')
 # Place a new picture of a cat or dog in 'single_prediction' folder and see if your model works
 import numpy as np
 from keras.preprocessing import image
-test_image = image.load_img('C:/Users/HP/Desktop/Accidents/fire/62590976.jpg', target_size = (64, 64))
+test_image = image.load_img('C:/Users/HP/Desktop/Accidents/fire/8b4c1d994e747fdf4ebe96590e5c5ac2--smoke-damage-fire-safety.jpg', target_size = (64, 64))
 # Add a 3rd Color dimension to match Model expectation
-#C:/Users/HP/Desktop/Accidents/fire/62590976.jpg
+#C:/Users/HP/Desktop/Accidents/fire/8b4c1d994e747fdf4ebe96590e5c5ac2--smoke-damage-fire-safety.jpg
 #C:/Users/HP/Desktop/Accidents/car/_b366f75e-0174-11eb-b32f-32d5f7e2c720.jpg
 test_image = image.img_to_array(test_image)
 # Add one more dimension to beginning of image array so 'Predict' function can receive it (corresponds to Batch, even if only one batch)
